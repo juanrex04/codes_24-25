bad4eb1619d9874e3327a9be20083bbe181cb527
Juan Restrepo <juan.computerscience@gmail.com>
2025-01-16 11:58:09 -0500

    explaning how change values

--- 6/list.py
+++ 6/list.py
@@ -1,5 +1,13 @@
 list_names = ["Juan","Salome","Santiago"]
 
+#print all the info
 print(list_names)
 
+#selection an item and print
 print(list_names[1])
+
+#checking if the list has a value
+if "Juan" in list_names:
+    print("the value is present in the list")
+else:
+    print("the value is not in the list")
