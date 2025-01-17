85474ab7b0e983da649a15ae02f680c93bc236dc
Juan Restrepo <juan.computerscience@gmail.com>
2025-01-17 15:02:10 -0500

    Explain how add items to a list

--- 6/list.py
+++ 6/list.py
@@ -6,8 +6,16 @@ print(list_names)
 #selection an item and print
 print(list_names[1])
 
+#change a item
+list_names[2] = "Maria Gabriela"
+print(list_names)
+
 #checking if the list has a value
-if "Juan" in list_names:
+if "juan" in list_names:
     print("the value is present in the list")
 else:
     print("the value is not in the list")
+    
+#append a new item
+list_names.append("Luciana")
+print(list_names)
