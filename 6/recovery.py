e7fb2829a136852a4b88742b0f7eb7c55dfb7ee3
Juan Restrepo <juan.computerscience@gmail.com>
2025-01-30 11:13:11 -0500

    Commit

---
+++ 6/recovery.py
@@ -0,0 +1,10 @@
+i = "continue"
+
+while i == "continue":
+    fruits = []
+    
+    option = str(input("Choice an option 1.Input a value in the list 2.Search a value 3.Remove a value 4.Print the list"))
+    
+    if option == "1":
+        
+        value = str(input("Write a fruit to keep in the list"))
