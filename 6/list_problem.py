a72b34f7c16d16557ef3fba4ad50a168401542c6
Juan Restrepo <juan.computerscience@gmail.com>
2025-02-26 08:57:56 -0500

    create function to add item specified position

--- 6/list_problem.py
+++ 6/list_problem.py
@@ -29,13 +29,22 @@ names_enterprice = ["Juan"]
 def new_item_last_position(newValue):
     names_enterprice.append(newValue)
     print (names_enterprice)
-    print("____________________________________")
-
+    print("-------------------------------------")
+    
+#function to add a new item in a specified position
+def new_item_specified_position():
+    print(names_enterprice)
+    newValue = str(input("Input the value that you want"))
+    position = int(input("Now, write which position do you want"))
+    names_enterprice.insert(position, newValue)
+    print("-------------------------------------") 
 
 while repeat == "y":
-    option = int(input("What option do you want? \n1.Input a new value in the last position \n2.Input new value specific position \n3.Remove a value in last position \n4.Remove a value using the index \n5.Remove a value using the item \n6.Print the list \n7.Close the program (y/n) \n"))
-    print("____________________________________")
+    option = str(input("What option do you want? \n1.Input a new value in the last position \n2.Input new value specific position \n3.Remove a value in last position \n4.Remove a value using the index \n5.Remove a value using the item \n6.Print the list \n7.Close the program (y/n) \n"))
+    print("-------------------------------------")   
     
-    if option == 1:
+    if option == "1":
         newValue = str(input("Input a new value for last position ")) 
         new_item_last_position(newValue)
+    elif option == "2":
+        new_item_specified_position()
