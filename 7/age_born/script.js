21a76808aa74a24150ea2357cbf0a866b47e7240
juanComputerScience <juan.computerscience@gmail.com>
2025-03-06 11:57:52 -0500

    Organize the problem to calculate a payment, modify JS and HTML structure

--- 7/age_born/script.js
+++ 7/age_born/script.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+let totalProduct = 0
+let result = 0
+
+function updateValue(){
+    totalProduct = parseInt(document.getElementById("total_product").value)
+}
+
+function totalPay(){
+    result = totalProduct * 100
+
+    document.getElementById("print_result").textContent = "Total to pay: " + result
+}
