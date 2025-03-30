5c85ee7801b5574fc6647b2813724bddcfbec659
juanrex04 <juanpix2404@gmail.com>
2025-03-30 15:31:51 -0500

    Update remember_6.py
    Start to teach some topics about python

--- 6/3 term/remember_6.py
+++ 6/3 term/remember_6.py
@@ -1,5 +1,19 @@
 #variables
 name = "Juan" #string always in double quotes
-age = 25 #int
-height = 1.75 #float
-is_student = True #bool
+age = 25 #int whole numbers
+height = 1.75 #float decimal numbers
+is_student = True #bool only use to values: true or false
+
+#keywords
+#keywords are reserved words in python and cannot be used as variable names
+
+print("Hello, my name is " + name + ".") #the print function is used to display output
+
+mail = str(input("Enter your email: ")) #input function is used to get user input, str() converts it to a string that means we use the casting function to convert the input to a string
+
+#control structure
+
+if age >= 18: #if statement is used to check a condition
+    print("You are an adult.")
+else: #else statement is used to execute a block of code if the condition is false
+    print("You are a minor.")
