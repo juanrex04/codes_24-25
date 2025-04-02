5a1d6e052a2f1f567595aec9d69ccf0c3e86b26e
juanrex04 <juanpix2404@gmail.com>
2025-04-02 08:55:07 -0500

    Explain the loop structure (for loop) and the comparison operator and logical operator

--- 6/3 term/remember_6.py
+++ 6/3 term/remember_6.py
@@ -13,7 +13,26 @@ mail = str(input("Enter your email: ")) #input function is used to get user inpu
 
 #control structure
 
+#the conditional statement modify the flow of the programn
 if age >= 18: #if statement is used to check a condition
     print("You are an adult.")
 else: #else statement is used to execute a block of code if the condition is false
     print("You are a minor.")
+
+#loops structure
+#the loops are used to repeat a block of code multiple times
+for i in range(5): #for loop is used to iterate over a sequence (list, tuple, dictionary, set, or string)
+    print("This is loop iteration number " + str(i) + ".")
+    
+#comparison operators and logical operators
+#comparison operators are used to compare two values and return a boolean result
+#logical operators are used to combine multiple conditions
+#comparison operators: == (equals to), !=(diffent to), > (greater than), < (less than), >= (greather than or equal to), <=(less than or equal to)
+#logical operators: and, or, not
+
+if age >= 18 and is_student == True: #and operator is used to check if both conditions are true
+    print("You are an adult student.")
+elif age >= 18 or is_student == True: #or operator is used to check if at least one condition is true
+    print("You are either an adult or a student.")
+else: #not operator is used to negate a condition 
+    print("You are neither an adult nor a student.")
