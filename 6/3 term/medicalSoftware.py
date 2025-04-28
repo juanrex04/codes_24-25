88d7244d84143023a71adde4738184bbc2083c08
juanrex04 <juanpix2404@gmail.com>
2025-04-28 09:05:02 -0500

    Create file to manage a simple medical software

---
+++ 6/3 term/medicalSoftware.py
@@ -0,0 +1,26 @@
+"""
+The idea of this software is to help 
+some medical professionals to manage 
+information and processes. We need to create a menu to know which option the doctor wants to choose.
+"""
+
+#functions to use in the software
+#function to register a patient
+def registerPatient():
+    print("Welcome to the patient registration")
+    
+#software menu
+print("Welcome, doctor!")
+print("Please choose an option:")
+print("1. Register a patient")
+print("2. Calculate BMI (Body Mass Index)")
+
+#create a variable to store the option chosen by the doctor
+userOption = int(input("Enter the option number: "))
+
+if userOption == 1:
+    #function to register a patient
+    print("Register a patient")
+elif userOption == 2:
+    #function to calculate BMI
+    print("Calculate BMI (Body Mass Index)")
